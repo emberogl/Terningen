02e2d925254dbbfbecb982440def851e0fdee332
emberogl <embernebulan@gmail.com>
2023-08-11 12:34:53 +0200

    Bruger switch istedet

--- Program.cs
+++ Program.cs
@@ -14,35 +14,32 @@
             int dice = random.Next(1, 7);
 
             // Kontrolstruktur
-            if (dice == 1) 
-            { 
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"You rolled a {dice}!");
-            }
-            else if (dice == 2) 
+            switch (dice)
             {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine($"You rolled a {dice}!"); 
-            }
-            else if (dice == 3)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"You rolled a {dice}!");
-            }
-            else if (dice == 4)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"You rolled a {dice}!");
-            }
-            else if (dice == 5)
-            {
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine($"You rolled a {dice}!");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine($"You rolled a 6!");
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"You rolled a {dice}!");
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"You rolled a {dice}!");
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"You rolled a {dice}!");
+                    break;
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"You rolled a {dice}!");
+                    break;
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"You rolled a {dice}!");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"You rolled a {dice}!");
+                    break;
             }
 
             // Afventer at gå tilbage
